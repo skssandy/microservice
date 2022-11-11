5a4f74b246f6281741b3ad6d968606b1be671aa3
skssandy <89083444+skssandy@users.noreply.github.com>
2022-11-11 18:13:51 +0530

    Update index.js

--- gateway/index.js
+++ gateway/index.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.get("/", (req, res) => {
 
-res.send("<h1> Sample application 1 </h1>");
+res.send("<h1> Sample application 2 </h1>");
 });
 
 app.listen(3000, () => {
 
-console.log("Listening to port 3000");
+console.log("Listening to port 4000");
 });
 
